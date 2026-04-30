fb8e96e72eda7eaf6ce610bfa223a95dd0a8e75c
name_0914 <51285903010@stu.ecnu.edu.cn>
2026-04-30 20:52:43 +0800

    assist2009

--- trainers/kt_trainer.py
+++ trainers/kt_trainer.py
@@ -4,8 +4,16 @@ import numpy as np
 import torch
 
 from torch.nn.functional import one_hot, binary_cross_entropy
+from sklearn import metrics
 
-from models.metrics import calc_binary_auc_acc
+
+def calc_binary_auc_acc(y_true, y_score, threshold=0.5):
+    auc = metrics.roc_auc_score(y_true=y_true, y_score=y_score)
+    acc = metrics.accuracy_score(
+        y_true=y_true,
+        y_pred=np.where(y_score >= threshold, 1, 0)
+    )
+    return auc, acc
 
 
 def _forward_for_batch(model_name, model, q, r, qshft):
